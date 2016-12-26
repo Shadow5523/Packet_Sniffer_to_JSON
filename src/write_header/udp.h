40e53b9313e2716b6ebd7cfbc7d692e0453d74a1
Shadow5523 <moonstar125y@gmail.com>
2016-12-27 01:29:21 +0900

    17th commit

--- src/write_header/udp.h
+++ src/write_header/udp.h
@@ -1,23 +1,16 @@
+#ifndef _UDP_H_
+#define _UDP_H_
+
+#include <stdio.h>
+#include <netinet/udp.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
 
 void write_udp_header(unsigned char *buffer,
 		      int size,
 		      struct sockaddr_in source,
-		      struct sockaddr_in dest, 
-		      FILE *logfile){
-  unsigned short iphdrlen;
+		      struct sockaddr_in dest,
+		      FILE *logfile);
+#endif _UDP_H_
 
-  struct iphdr *iph = (struct iphdr *)(buffer + sizeof(struct ethhdr));
-  iphdrlen = (iph -> ihl) * 4;
-
-  struct udphdr *udph = (struct udphdr *)(buffer + iphdrlen + sizeof(struct ethhdr));
-  write_ip_header(buffer, size, source, dest, logfile);
-
-  fprintf(logfile, "\"udp\":{");
-  fprintf(logfile, "\"src\":\"%d\",", ntohs(udph -> source));
-  fprintf(logfile, "\"dst\":\"%d\",", ntohs(udph -> dest));
-  fprintf(logfile, "\"len\":\"%d\",", ntohs(udph -> len));
-  fprintf(logfile, "\"check\":\"%d\"", ntohs(udph -> check));
-  fprintf(logfile, "}");
-  
-}
 
